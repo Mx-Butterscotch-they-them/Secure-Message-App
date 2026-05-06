5b95c9535723323a163f8d4066aa7b4342e9a9d6
Mx-Butterscotch-they-them <sperson7997@gmail.com>
2026-05-06 17:58:45 -0400

    Project Meson set up

---
+++ build/meson.debug.linux.x86_64.Local/EncryptionApp.p/EncryptionApp.cpp.o.d
@@ -0,0 +1,3 @@
+/home/lex/eclipse-workspace1/EncryptionApp/build/meson.debug.linux.x86_64.Local/EncryptionApp.p/.sc6858935543047310570.o: \
+ /home/lex/eclipse-workspace1/EncryptionApp/.sc5593427739633410725.cpp \
+ /usr/include/stdc-predef.h
